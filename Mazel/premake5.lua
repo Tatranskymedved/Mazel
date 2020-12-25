77959cf76b593954dad811d6c3c1fa4e63143bd6
Jan Urbanec <UrbanecJ@seznam.cz>
2020-12-25 14:14:02 +0100

    Added premake to build projects/build settings, to actually build correctly the engine+sandbox.

---
+++ Mazel/premake5.lua
@@ -0,0 +1,99 @@
+workspace "Mazel"
+    architecture "x64"
+    configurations { "Debug", "Release", "Dist" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Mazel"
+    location "Mazel"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+
+        defines
+        {
+            "MZ_PLATFORM_WINDOWS",
+            "MZ_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines { "MZ_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "MZ_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "MZ_DIST" }
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "Mazel/vendor/spdlog/include",
+        "Mazel/src"
+    }
+
+    links
+    {
+        "Mazel"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+
+        defines
+        {
+            "MZ_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines { "MZ_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "MZ_RELEASE" }
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines { "MZ_DIST" }
+        optimize "On"
--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Game engine based on the Cherno's Hazel
 # External libraries
 
 - Spdlog: https://github.com/gabime/spdlog
+- Premake5: https://github.com/premake/premake-core
