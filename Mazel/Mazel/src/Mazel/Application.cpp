a3f4192ad8a6de1c1c0c25f60db7254314fd253f
Jan Urbanec <UrbanecJ@seznam.cz>
2020-12-26 14:20:10 +0100

    Linked GLFW window with Mazel's event system.

--- Mazel/Mazel/src/Mazel/Application.cpp
+++ Mazel/Mazel/src/Mazel/Application.cpp
@@ -1,25 +1,41 @@
 #include "mzpch.h"
 #include "Application.h"
 
-#include "Mazel/Events/ApplicationEvent.h"
 #include "Mazel/Log.h"
 
 namespace Mazel {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		MZ_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
-		while (true)
+		while (m_Running)
 		{
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Mazel/Mazel/src/Platform/Windows/WindowsWindow.cpp
+++ Mazel/Mazel/src/Platform/Windows/WindowsWindow.cpp
@@ -1,9 +1,18 @@
 #include "mzpch.h"
 #include "WindowsWindow.h"
 
+#include "Mazel/Events/ApplicationEvent.h"
+#include "Mazel/Events/MouseEvent.h"
+#include "Mazel/Events/KeyEvent.h"
+
 namespace Mazel
 {
 	static bool s_GLFWInitialized = false;
+
+	static void GLFWErrorCallback(int error_code, const char* description)
+	{
+		MZ_CORE_ERROR("GLFW Error ({0}): {1}", error_code, description);
+	}
 
 	Window* Window::Create(const WindowProps& props)
 	{
@@ -32,6 +41,8 @@ namespace Mazel
 			int isInit = glfwInit();
 			MZ_CORE_ASSERT(isInit, "Could not initialize GLFW!");
 
+			glfwSetErrorCallback(GLFWErrorCallback);
+
 			s_GLFWInitialized = true;
 		}
 
@@ -39,6 +50,90 @@ namespace Mazel
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowUserPointer(m_Window, &m_Data);
 		SetVSync(true);
+
+#pragma region [GLFW callbacks]
+		glfwSetWindowSizeCallback(m_Window, [](GLFWwindow* window, int width, int height)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			data.Width = width;
+			data.Height = height;
+
+			WindowResizeEvent ev(width, height);
+			data.EventCallback(ev);
+		});
+
+		glfwSetWindowCloseCallback(m_Window, [](GLFWwindow* window)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			WindowCloseEvent ev;
+			data.EventCallback(ev);
+		});
+
+		glfwSetKeyCallback(m_Window, [](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					KeyPressedEvent ev(key, 0);
+					data.EventCallback(ev);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					KeyReleasedEvent ev(key);
+					data.EventCallback(ev);
+					break;
+				}
+				case GLFW_REPEAT:
+				{
+					KeyPressedEvent ev(key, 1);
+					data.EventCallback(ev);
+					break;
+				}
+			}
+		});
+
+		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			switch (action)
+			{
+				case GLFW_PRESS:
+				{
+					MouseButtonPressedEvent ev(button);
+					data.EventCallback(ev);
+					break;
+				}
+				case GLFW_RELEASE:
+				{
+					MouseButtonReleasedEvent ev(button);
+					data.EventCallback(ev);
+					break;
+				}
+			}
+		});
+
+		glfwSetScrollCallback(m_Window, [](GLFWwindow* window, double xOffset, double yOffset)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseScrolledEvent ev((float)xOffset, (float)yOffset);
+			data.EventCallback(ev);
+		});
+
+		glfwSetCursorPosCallback(m_Window, [](GLFWwindow* window, double xPos, double yPos)
+		{
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			MouseMovedEvent ev((float)xPos, (float)yPos);
+			data.EventCallback(ev);
+		});
+#pragma endregion [GLFW callbacks]
+
 	}
 
 	void WindowsWindow::Shutdown()
